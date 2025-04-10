f686d9dd45e14b8e5999375bc7b2e360236ab154
sonia <sonianr03@gmail.com>
2025-04-10 18:48:17 +0200

    feat: phone design

--- phone/script.js
+++ phone/script.js
@@ -1,9 +1,15 @@
-const socket = io(); // Conectar con el servidor
+const socket = io();
 
 document.addEventListener("DOMContentLoaded", function () {
     const nextButton = document.getElementById("next-page");
     nextButton.addEventListener("click", function () {
         socket.emit("next-page");
+    });
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+    const nextButton = document.getElementById("prev-page");
+    nextButton.addEventListener("click", function () {
+        socket.emit("prev-page");
     });
 });
