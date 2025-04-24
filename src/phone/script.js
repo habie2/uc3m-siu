323f1ada9c1bbf5de214ded9bc0c343ef70b84cf
javichu <100472296@alumnos.uc3m.es>
2025-04-24 18:45:58 +0200

    quitado leer en boton prev

--- src/phone/script.js
+++ src/phone/script.js
@@ -180,7 +180,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const nextButton = document.getElementById("prev-page");
   nextButton.addEventListener("click", function () {
     socket.emit("prev-page");
-    socket.emit("que-leo");
     resetInactivityTimer();
   });
 });
