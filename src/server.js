7bd3336d8fc83df0721d65abd9ea808a7ad147ff
sonia <sonianr03@gmail.com>
2025-04-24 22:05:39 +0200

    finiquitao

--- src/server.js
+++ src/server.js
@@ -100,5 +100,5 @@ io.on('connection', (socket) => {
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`🚀 Servidor corriendo en https://localhost:${PORT}`);
 });
